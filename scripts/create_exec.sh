528196eee124209f0255998b520b91e87585c5da
Jachym Holecek <holecekjachym5@gmail.com>
2023-01-28 22:00:05 +0100

    Added script for generating executable

---
+++ scripts/create_exec.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Bash script to build a single executable to run the app
+# TODO: Test if executable works correctly
+cd .. && cd src/
+pyinstaller --noconfirm --onefile --console --name "yt-downloader" --add-binary "$(readlink -f file.py):." --add-binary "$(readlink -f downloader.py):." --add-binary "$(readlink -f arguments.py):." --hidden-import "pytube" --hidden-import "moviepy.editor" "$(readlink -f __init__.py)"
