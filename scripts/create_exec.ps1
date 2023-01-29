061a33b27bb012caad22ead0a06650361c30becc
Jachym Holecek <holecekjachym5@gmail.com>
2023-01-29 15:44:59 +0100

    Updated and tested create_exec.ps1

--- scripts/create_exec.ps1
+++ scripts/create_exec.ps1
@@ -3,4 +3,4 @@
 Set-Location ..
 Set-Location src
 
-pyinstaller --noconfirm --onefile --console --name "yt-downloader" --add-binary (Get-Item -Path '.\file.py').FullName+":." --add-binary (Get-Item -Path '.\downloader.py').FullName+":." --add-binary (Get-Item -Path '.\arguments.py').FullName+":." --hidden-import "pytube" --hidden-import "moviepy.editor" (Get-Item -Path '.\__init__.py').FullName
+pyinstaller --noconfirm --onefile --console --name "yt-downloader" --add-binary "file.py;." --add-binary "downloader.py;." --add-binary "arguments.py;." --hidden-import "pytube" --hidden-import "moviepy.editor" "__init__.py"
