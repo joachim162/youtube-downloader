72a271a1d1645378f430eb2acaef70d601197549
Jáchym Holeček <88273448+joachim162@users.noreply.github.com>
2023-02-09 11:11:36 +0100

    Update create_exec.sh

--- scripts/create_exec.sh
+++ scripts/create_exec.sh
@@ -2,4 +2,4 @@
 
 # Bash script to build a single executable to run the app
 cd .. && cd src/
-pyinstaller --noconfirm --onefile --console --name0 "yt-downloader" --add-binary "$(readlink -f file.py):." --add-binary "$(readlink -f downloader.py):." --add-binary "$(readlink -f arguments.py):." --hidden-import "pytube" --hidden-import "moviepy.editor" "$(readlink -f __init__.py)"
+pyinstaller --noconfirm --onefile --console --name "yt-downloader" --add-binary "$(readlink -f file.py):." --add-binary "$(readlink -f downloader.py):." --add-binary "$(readlink -f arguments.py):." --hidden-import "pytube" --hidden-import "moviepy.editor" "$(readlink -f __init__.py)"
